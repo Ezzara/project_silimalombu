556c97008179dc97eacb26f09a353d69e0447129
Aditya Samiaji <72873853+RollRex@users.noreply.github.com>
2022-06-20 07:05:37 +0700

    Update userapp.blade.php

---
+++ resources/views/layouts/userapp.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel="stylesheet" href="admin.css">
+    <script src='main.js'></script>
+    <script src="https://kit.fontawesome.com/510de3573f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <style type="text/css">
+
+    </style>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+        <a class="navbar-brand text-black font-weight-bold ml-5" href="#">Ecovillage <br> Silimalombu</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item px-3">
+                    <a class="nav-link active" href="#">HOME <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">PRODUK</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">NOTIFIKASI</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">TENTANG</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">Q&A</a>
+                </li>
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+                <a href=""><i class="fas fa-user fa-2x text-white px-3"></i></a>
+                <a href=""> <i class="fas fa-cart-arrow-down fa-2x text-white px-3"></i></a>
+                <a href="" class="nav-link text-white lead font-weight-bold px-3">Login</a>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</body>
