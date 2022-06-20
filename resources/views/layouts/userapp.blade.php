7ad47f83674e07cb8b60ec80bc205b345d547504
Aditya Samiaji <72873853+RollRex@users.noreply.github.com>
2022-06-20 07:11:37 +0700

    Update userapp.blade.php

--- resources/views/layouts/userapp.blade.php
+++ resources/views/layouts/userapp.blade.php
@@ -7,7 +7,7 @@
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="css/admin.css">
     <script src='main.js'></script>
     <script src="https://kit.fontawesome.com/510de3573f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
