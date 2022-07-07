78050f5fce86adc5fdac2a4b17686d28c751acac
Aditya Samiaji <72873853+RollRex@users.noreply.github.com>
2022-07-07 19:33:42 +0700

    minor

--- resources/views/layouts/userapp.blade.php
+++ resources/views/layouts/userapp.blade.php
@@ -59,7 +59,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-        <a class="navbar-brand text-black font-weight-bold ml-5" href="{{route('welcome')}}"><img class="rounded-circle" src="{{ asset('image/logo.png') }}" alt="" style="width:65px; height:65px;"></a>
+        <a class="navbar-brand text-black font-weight-bold ml-5" href="{{route('welcome')}}"><img class="rounded-circle" src="{{ asset('image/logo.png') }}" alt="" style="width:80px; height:80px;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
